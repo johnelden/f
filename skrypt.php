3a257467874f59d2dda3210b65760f1055eacbdc
johnelden <Mateusz.modlodajny@gmail.com>
2026-03-02 13:33:54 +0100

    Add files via upload

---
+++ skrypt.php
@@ -0,0 +1,40 @@
+ 
+ 
+ 
+ <?php
+ 
+ $koszyk = 0;
+ 
+ $cena_soku = 7.5;
+ $cena_czekolady = 25;
+
+$koszyk = $cena_soku + $cena_czekolady;
+
+ print $koszyk;
+
+ $cena_dobrej_czekolady = $cena_czekolady * 1.23;
+
+ $koszyk = $koszyk + $cena_dobrej_czekolady;
+print "<br / >" .$koszyk .(" cena koszyka z dobrą czekoladą"); 
+print "<br / >" .$cena_dobrej_czekolady .(" εϞΖϏεͱε");
+
+
+
+
+$a = 5;
+$b = 10;
+print ("suma wartości") .($a + $b); 
+print ("różnica wartości") .($a - $b);
+print (" iloczyn wartości") .($a * $b); 
+print ("JB😂😁😁😁👍");  
+
+
+$a = 12;
+$b = 5; 
+$a -= 5;
+print $a;
+ ?> 
+
+
+
+εϞΖϏεͱε
